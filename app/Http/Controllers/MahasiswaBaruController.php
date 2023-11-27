99f53733b0724e7b8607c127cb0292a3defa7f07
UcupBoys <yusufrahmana14@gmail.com>
2023-11-27 23:10:02 +0700

    first commit

---
+++ app/Http/Controllers/MahasiswaBaruController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\mahasiswaBaru;
+use Illuminate\Http\Request;
+
+class MahasiswaBaruController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(mahasiswaBaru $mahasiswaBaru)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(mahasiswaBaru $mahasiswaBaru)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, mahasiswaBaru $mahasiswaBaru)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(mahasiswaBaru $mahasiswaBaru)
+    {
+        //
+    }
+}
