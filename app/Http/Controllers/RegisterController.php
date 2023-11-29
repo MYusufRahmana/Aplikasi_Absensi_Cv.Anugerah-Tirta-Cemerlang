1f885a0688ffb4b203df623b1d850294094b062b
UcupBoys <yusufrahmana14@gmail.com>
2023-11-29 22:03:28 +0700

    Fix Database and make Breeze for authentication

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\register;
+use App\Http\Requests\StoreregisterRequest;
+use App\Http\Requests\UpdateregisterRequest;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreregisterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(register $register)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(register $register)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateregisterRequest $request, register $register)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(register $register)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_29_144226_create_registers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->integer('no');
+            $table->string('Nama');
+            $table->string('Gender');
+            $table->string('Sekolah');
+            $table->string('Health');
+            $table->date('Tgl');
+            $table->string('Kelas');
+            $table->string('Ortu');
+            $table->string('Alamat');
+            $table->string('Hp');
+            $table->string('email');
+            $table->string('gbr');
+            $table->timestamps();
+            $table->string('password');
+            $table->integer('status');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registers');
+    }
+};
